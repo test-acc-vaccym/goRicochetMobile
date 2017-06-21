b1220bdf851c1cc742341fe947473ec0e1ee15db
Dan Ballard <dan@mindstab.net>
2017-06-21 07:27:13 -0700

    imports from reverse example

---
+++ InitActivity.go
@@ -0,0 +1,15 @@
+package go_ricochet_mobile
+
+import (
+	"Java/android/databinding/DataBindingUtil"
+	"Java/android/os"
+	"Java/android/support/v7/app"
+	gopkg "Java/RicochetMobile"
+	rlayout "Java/RicochetMobile/R/layout"
+	"Java/RicochetMobile/databinding"
+	"Java/RicochetMobile/ActivityInitBinding"
+)
+
+type InitActivity struct {
+
+}
