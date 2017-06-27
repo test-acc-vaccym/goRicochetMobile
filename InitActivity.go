c3335cc21aab2e703859a43f6ce5f1fdad4ae84b
Dan Ballard <dan@mindstab.net>
2017-06-27 08:13:53 -0700

    strip and add dummy place holder as go driven app is not currently possible

--- InitActivity.go
+++ InitActivity.go
@@ -3,16 +3,8 @@
 package go_ricochet_mobile
 
 import (
-	"Java/android/databinding/DataBindingUtil"
-	"Java/android/os"
-	"Java/android/support/v7/app"
-	gopkg "Java/RicochetMobile"
-	rlayout "Java/RicochetMobile/R/layout"
-	"Java/RicochetMobile/databinding"
-	"Java/RicochetMobile/ActivityInitBinding"
 )
 
-type InitActivity struct {
-	app.AppCompatActivity
-	binding databinding.ActivityInitBinding
+func GetHello() string {
+	return "Hello"
 }
